2a8bcce69d4b99addede64effe15d23853c9e4da
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-08-15 08:46:57 +0700

    feat: Add env and cors

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 // ===================================
 const routerNavigation = require("./src");
 // ===================================
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
+// CORS
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 // ===================================
 app.use("/", routerNavigation);
@@ -19,6 +32,8 @@ app.get("*", (request, response) => {
   response.status(404).send("Path Not Found !");
 });
 
-app.listen(3001, "127.0.0.1", () => {
-  console.log("Express app is listening on host: 127.0.0.1 and port: 3001");
+app.listen(process.env.PORT, process.env.IP_ADDRESS, () => {
+  console.log(
+    `Express app is listening on host: ${process.env.IP_ADDRESS} and port: ${process.env.PORT}`
+  );
 });
--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "pos_food",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((error) => {
