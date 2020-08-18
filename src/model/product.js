27959cc5f70720ba6eaecf7181392bf880e6898b
Galuh Prahadi Gumelar <40691793+galuhprahadi96@users.noreply.github.com>
2020-08-18 08:20:59 +0700

    Fix : fix bug product

--- src/model/product.js
+++ src/model/product.js
@@ -2,29 +2,16 @@ const connection = require("../config/mysql");
 
 module.exports = {
   // ambil semua data product
-  getProduct: (limit = 1, offset = 0, name = "product_id", sort = "ASC") => {
+  getProduct: (limit, offset, name, sort) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT product_id, category_name, product_name, product_image,product_price, status, product_created_at, product_update_at FROM product INNER JOIN category ON product.id_category=category.category_id ORDER BY ${name} ${sort} LIMIT ${limit} OFFSET ${offset}`,
+        `SELECT product_id, category_name, product_name, product_image,product_price, status, product_created_at, product_update_at FROM product LEFT JOIN category ON product.id_category=category.category_id ORDER BY ${name} ${sort} LIMIT ${limit} OFFSET ${offset}`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error));
         }
       );
     });
   },
-
-  // // ambil semua data product
-  // getProduct: (limit = 1, offset = 0) => {
-  //   return new Promise((resolve, reject) => {
-  //     connection.query(
-  //       "SELECT product_id, category_name, product_name, product_image, status, product_created_at, product_update_at FROM product INNER JOIN category ON product.id_category=category.category_id ORDER BY product.product_id ASC LIMIT ? OFFSET ? ",
-  //       [limit, offset],
-  //       (error, result) => {
-  //         !error ? resolve(result) : reject(new Error(error));
-  //       }
-  //     );
-  //   });
-  // },
 
   // hitung jumlah data
   getProductCount: () => {
