a927994e20a1bd49ed1d21014619fe2960018ffa
Galuh Prahadi Gumelar <40691793+galuhprahadi96@users.noreply.github.com>
2020-09-03 08:30:56 +0700

    add endpoint users

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const product = require("./routes/product");
 const category = require("./routes/category");
 const order = require("./routes/order");
 const history = require("./routes/history");
+const users = require("./routes/users");
 // middle
 route.use("/product", product);
 route.use("/category", category);
 route.use("/order", order);
 route.use("/history", history);
+route.use("/users", users);
 
 module.exports = route;
