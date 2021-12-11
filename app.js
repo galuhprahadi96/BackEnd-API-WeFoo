4e91d54e7adbac2f21a9d91ab4326b243091bb79
Galuh Prahadi Gumelar <40691793+gumelarid@users.noreply.github.com>
2021-12-11 23:45:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(express.static("uploads"));
 
-// app.use((request, response, next) => {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Request-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
+app.use((request, response, next) => {
+response.header("Access-Control-Allow-Origin", "*");
+response.header(
+"Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept, Authorization"
+   );
+   next();
+ });
 app.use(cors());
 app.use("/", routerNavigation);
 
