131cb3927ddc8062b1ea6bd4b65b6cca7ed073f3
gumelarid <galuhprahadi96@gmail.com>
2021-12-14 19:01:44 +0700

    'change-cors'

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const routerNavigation = require("./src");
 
 const app = express();
-
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,22 +14,20 @@ app.use(morgan("dev"));
 app.use(express.static("uploads"));
 
 app.use((request, response, next) => {
-response.header("Access-Control-Allow-Origin", "*");
-response.header(
-"Access-Control-Allow-Headers",
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
     "Origin, X-Request-With, Content-Type, Accept, Authorization"
-   );
-   next();
- });
-app.use(cors());
+  );
+  next();
+});
+
 app.use("/", routerNavigation);
 
 app.get("*", (request, response) => {
   response.status(404).send("Path Not Found !");
 });
 
-app.listen(process.env.PORT, process.env.IP_ADDRESS, () => {
-  console.log(
-    `Express app is listening on host: ${process.env.IP_ADDRESS} and port: ${process.env.PORT}`
-  );
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
 });
